fdb4618490ca33f1e3d2128215ecacb47fb27b4c
pron <ron.pressler@gmail.com>
2013-04-13 21:43:08 +0300

    Changed AFunction to IFn in ClojureRetransform

--- src/main/java/co/paralleluniverse/fibers/instrument/ClojureRetransform.java
+++ src/main/java/co/paralleluniverse/fibers/instrument/ClojureRetransform.java
@@ -4,7 +4,8 @@
  */
 package co.paralleluniverse.fibers.instrument;
 
-import clojure.lang.AFunction;
+import clojure.lang.IFn;
+import co.paralleluniverse.common.util.Exceptions;
 import co.paralleluniverse.fibers.Instrumented;
 import co.paralleluniverse.fibers.SuspendExecution;
 import co.paralleluniverse.strands.SuspendableCallable;
@@ -18,32 +19,35 @@ import org.objectweb.asm.Type;
  * @author pron
  */
 public class ClojureRetransform {
-    public static void retransform(AFunction fn) throws UnmodifiableClassException {
+    public static void retransform(IFn fn) throws UnmodifiableClassException {
         final Class clazz = fn.getClass();
         MethodDatabase.ClassEntry entry = Retransform.db.getClassEntry(Type.getInternalName(clazz));
         entry.setRequiresInstrumentation(true);
         Method[] methods = clazz.getMethods();
-        for(Method method : methods) {
-            if(method.getName().equals("invoke") || method.getName().equals("doInvoke")) {
+        for (Method method : methods) {
+            if (method.getName().equals("invoke") || method.getName().equals("doInvoke")) {
                 // System.out.println("X: request retransform " + Type.getInternalName(clazz) + "." + method.getName() + Type.getMethodDescriptor(method));
                 entry.set(method.getName(), Type.getMethodDescriptor(method), true);
             }
         }
         Retransform.retransform(fn.getClass());
     }
-    
-    public static SuspendableCallable wrap(final AFunction fn) {
-        if(!isInstrumented(fn.getClass()))
+
+    public static SuspendableCallable wrap(final IFn fn) {
+        if (!isInstrumented(fn.getClass()))
             throw new IllegalArgumentException("Function " + fn + " has not been instrumented");
         return new SuspendableCallable() {
-
             @Override
             public Object run() throws SuspendExecution, InterruptedException {
-                return fn.call();
+                try {
+                    return fn.call();
+                } catch (Exception e) {
+                    throw Exceptions.rethrow(e);
+                }
             }
         };
     }
-    
+
     private static boolean isInstrumented(Class clazz) {
         return clazz.isAnnotationPresent(Instrumented.class);
     }
