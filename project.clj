8249c493a3551938ca5969065942169de69718cd
pron <ron.pressler@gmail.com>
2013-04-30 23:22:13 +0300

    project.clj

--- project.clj
+++ project.clj
@@ -10,7 +10,7 @@
   :javac-options     ["-target" "1.7" "-source" "1.7"]
   :repositories {"project" "file:lib"}
   :test-selectors {:selected :selected}
-  :dependencies [[org.clojure/clojure "1.5.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [jsr166e/jsr166e "0.1"]
                  [co.paralleluniverse/quasar "0.1-SNAPSHOT"]
                  [com.yammer.metrics/metrics-core "2.0.2"]
@@ -22,13 +22,13 @@
                  [org.clojure/core.match "0.2.0-alpha12"]]
   :jvm-opts ^:replace ["-server"
                        "-javaagent:/Users/pron/Projects/quasar/build/libs/quasar-0.1-SNAPSHOT.jar"]
-  :profiles {:dev 
+  :profiles {:dev
              {:plugins [[codox "0.6.4"]
                         [lein-marginalia "0.7.1"]]
               :codox {:include co.paralleluniverse.pulsar
                       :output-dir "docs"}
-              :jvm-opts ["-ea" 
-                         ;"-Dco.paralleluniverse.debugMode=true" 
+              :jvm-opts ["-ea"
+                         ;"-Dco.paralleluniverse.debugMode=true"
                          "-Dco.paralleluniverse.lwthreads.verifyInstrumentation=true"
                          "-Dco.paralleluniverse.globalFlightRecorder=true" "-Dco.paralleluniverse.flightRecorderDumpFile=pulsar.log"]
               :global-vars {*warn-on-reflection* true}}})
