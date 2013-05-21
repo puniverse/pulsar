a514ae2d9e966a8024c506fa823d14120cc10503
pron <ron.pressler@gmail.com>
2013-05-21 12:35:13 +0300

    fix travis post build script...

--- travis/post_build.sh
+++ travis/post_build.sh
@@ -6,16 +6,24 @@ email="info@paralleluniverse.co"
 username="PU Bot"
 site_dir=docs/_site
 
+function error_exit
+{
+	echo -e "\e[01;31m$1\e[00m" 1>&2
+	exit 1
+}
+
 if [ "$TRAVIS_BRANCH" == "master" ]; then
+	echo -e "Installing Jekyll...\n"
+	gem install jekyll
 	# build site
 	echo -e "Building Jekyll site...\n"
 	cd docs
 	jekyll build || error_exit "Error building Jekyll site"
 	cd ..
 
-	echo -e "Building Jekyll site done. Updating gh-pages...\n"
+	echo -e "Updating gh-pages...\n"
     # Any command that using GH_OAUTH_TOKEN must pipe the output to /dev/null to not expose your oauth token
-    git submodule add -b gh-pages https://${GH_OAUTH_TOKEN}@github.com/${GH_OWNER}/${GH_PROJECT_NAME} site > /dev/null 2>&1
+    git submodule add -b gh-pages https://${GH_OAUTH_TOKEN}@github.com/$TRAVIS_REPO_SLUG site > /dev/null 2>&1
     cd site
     if git checkout gh-pages; then 
     	git checkout -b gh-pages
@@ -28,7 +36,7 @@ if [ "$TRAVIS_BRANCH" == "master" ]; then
     git config user.name $username
     git commit -am "Travis build $TRAVIS_BUILD_NUMBER, commit $TRAVIS_COMMIT, pushed to gh-pages"
     # Any command that using GH_OAUTH_TOKEN must pipe the output to /dev/null to not expose your oauth token
-    git push https://${GH_OAUTH_TOKEN}@github.com/${GH_OWNER}/${GH_PROJECT_NAME} HEAD:gh-pages > /dev/null 2>&1 || error_exit "Error updating gh-pages"
+    git push https://${GH_OAUTH_TOKEN}@github.com/$TRAVIS_REPO_SLUG HEAD:gh-pages > /dev/null 2>&1 || error_exit "Error updating gh-pages"
 
 	#git checkout -B gh-pages
 	#git add -f dist/.
