bac7232771a573d23f7255735619553638b28632
pron <ron.pressler@gmail.com>
2013-05-21 13:34:19 +0300

    fix api doc generation in post build

--- travis/post_build.sh
+++ travis/post_build.sh
@@ -16,8 +16,9 @@ if [ "$TRAVIS_BRANCH" == "master" ]; then
 	echo -e "Installing Jekyll...\n"
 	gem install jekyll
 
-	echo -e "Generating API docs...\n"
 	if [ -z $GEN_APIDOCS ]; then
+		echo -e "Generating API docs...\n"
+		echo -e "Running: $GEN_APIDOCS\n"
 		eval $GEN_APIDOCS
 	fi
 
