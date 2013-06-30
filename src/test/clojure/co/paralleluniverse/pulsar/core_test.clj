9ccef77c0093d23847ad382b8c29446ee9f4f34d
pron <ron.pressler@gmail.com>
2013-07-01 00:10:38 +0300

    Timing changes for Travis

--- src/test/clojure/co/paralleluniverse/pulsar/core_test.clj
+++ src/test/clojure/co/paralleluniverse/pulsar/core_test.clj
@@ -207,9 +207,9 @@
                    ch2 (channel)
                    ch3 (channel)
                    topic (topic)
-                   f1 (spawn-fiber #(list (rcv ch1 100 :ms) (rcv ch1 100 :ms) (rcv ch1 100 :ms)))
-                   f2 (spawn-fiber #(list (rcv ch2 100 :ms) (rcv ch2 100 :ms) (rcv ch2 100 :ms)))
-                   f3 (spawn-fiber #(list (rcv ch3 100 :ms) (rcv ch3 100 :ms) (rcv ch3 100 :ms)))]
+                   f1 (spawn-fiber #(list (rcv ch1 200 :ms) (rcv ch1 200 :ms) (rcv ch1 200 :ms)))
+                   f2 (spawn-fiber #(list (rcv ch2 200 :ms) (rcv ch2 200 :ms) (rcv ch2 200 :ms)))
+                   f3 (spawn-fiber #(list (rcv ch3 200 :ms) (rcv ch3 200 :ms) (rcv ch3 200 :ms)))]
                (subscribe topic ch1)
                (subscribe topic ch2)
                (subscribe topic ch3)
