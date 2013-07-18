63d83ab778e99e1eba5ff45c466ebead5753e10b
eitan <eitan@paralleluniverse.co>
2013-07-18 18:16:49 +0300

    -

--- src/test/clojure/co/paralleluniverse/pulsar/examples/distributed/ping.clj
+++ src/test/clojure/co/paralleluniverse/pulsar/examples/distributed/ping.clj
@@ -5,6 +5,7 @@
 ;; This example is intended to be a line-by-line translation of the canonical
 ;; Erlang [ping-pong example](http://www.erlang.org/doc/getting_started/conc_prog.html#id67347),
 ;; so it is not written in idiomatic Clojure.
+;; for running see comment in pong.clj
 
 (defsfn ping [n]
   (if (== n 0)
@@ -19,5 +20,4 @@
 
 (defn -main []
   (spawn ping 3)
-;  (prn (whereis "pong"))
   :ok)
--- src/test/clojure/co/paralleluniverse/pulsar/examples/distributed/pong.clj
+++ src/test/clojure/co/paralleluniverse/pulsar/examples/distributed/pong.clj
@@ -5,6 +5,10 @@
 ;; This example is intended to be a line-by-line translation of the canonical
 ;; Erlang [ping-pong example](http://www.erlang.org/doc/getting_started/conc_prog.html#id67347),
 ;; so it is not written in idiomatic Clojure.
+;; run this with
+;; lein with-profile cluster update-in :jvm-opts conj '"-Dgalaxy.nodeId=1"' '"-Dgalaxy.port=7051"' '"-Dgalaxy.slave_port=8051"' -- run -m co.paralleluniverse.pulsar.examples.distributed.pong
+;; and after that
+;; lein with-profile cluster update-in :jvm-opts conj '"-Dgalaxy.nodeId=2"' '"-Dgalaxy.port=7052"' '"-Dgalaxy.slave_port=8052"' -- run -m co.paralleluniverse.pulsar.examples.distributed.ping
 
 (defsfn pong []
   (receive
