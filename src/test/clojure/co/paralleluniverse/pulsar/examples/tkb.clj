c4ea585dbe848ecaaab62da036fd3a64a564a709
pron <ron.pressler@gmail.com>
2014-02-18 19:58:11 +0200

    -

--- src/test/clojure/co/paralleluniverse/pulsar/examples/tkb.clj
+++ src/test/clojure/co/paralleluniverse/pulsar/examples/tkb.clj
@@ -5,32 +5,6 @@
   (:require [co.paralleluniverse.pulsar.rx :as rx]))
 
 
-(let [t (topic)
-      x (promise)
-      f1 (spawn-fiber (fn []
-                        (let [c (subscribe! t (channel 0))]
-                          (loop []
-                            (when-let [m (rcv c)]
-                              (println "=>" (+ m @x))
-                              (recur))))))
-      f2 (spawn-fiber (sfn []
-                        (let [c (subscribe! t (int-channel 3))]
-                          (try
-                            (loop []
-                              (println "->" (* @x (rcv-int c)))
-                              (recur))
-                            (catch ReceivePort$EOFException e)))))]
-  (spawn-fiber (fn []
-                 (sleep 1000)
-                 (deliver x 5)))
-  (spawn-fiber (fn []
-                    (dotimes [i 10]
-                      (sleep 100)
-                      (snd t (rand-int 100)))
-                    (close! t)))
-
-  (join [f1 f2]))
-
 (defn -main[])
 
 
