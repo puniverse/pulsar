f4b80a94dd6518ddcc2a64bad3b8c23c1a19d477
pron <ron.pressler@gmail.com>
2014-02-19 12:35:13 +0200

    -

--- src/test/clojure/co/paralleluniverse/pulsar/examples/tkb.clj
+++ src/test/clojure/co/paralleluniverse/pulsar/examples/tkb.clj
@@ -4,19 +4,6 @@
   (:refer-clojure :exclude [promise await])
   (:require [co.paralleluniverse.pulsar.rx :as rx]))
 
-;(alter-var-root #'*compiler-options* (fn [_] {:disable-locals-clearing true}))
-(println "======" clojure.core/*compiler-options*)
-(defn foo []
-  (let [a "hello"]
-    (^:once fn* [] (println "!!!" a))))
-
-(defn bar []
-     (let [f (foo)]
-       (f)
-       (f)))
-
-(bar)
-
 (defn -main[])
 
 
