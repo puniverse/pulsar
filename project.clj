fac0eb64a8245cb6702da1c28a7c380ca5e1dfd7
pron <ron.pressler@gmail.com>
2015-06-01 15:52:03 +0300

    new dev iteration

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(def quasar-version "0.7.0")
-(defproject co.paralleluniverse/pulsar "0.7.0"
+(def quasar-version "0.7.1-SNAPSHOT")
+(defproject co.paralleluniverse/pulsar "0.7.1-SNAPSHOT"
   :description "A Clojure lightweight thread, asynchronous programming, and actor library"
   :url "http://github.com/puniverse/pulsar"
   :licenses [{:name "Eclipse Public License - v 1.0" :url "http://www.eclipse.org/legal/epl-v10.html"}
