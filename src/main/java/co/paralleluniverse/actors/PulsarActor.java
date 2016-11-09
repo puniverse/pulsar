7c25878fef13fdb2e9972a1846b19a6b001aa654
pron <ron.pressler@gmail.com>
2016-11-09 18:04:06 +0200

    -

--- src/main/java/co/paralleluniverse/actors/PulsarActor.java
+++ src/main/java/co/paralleluniverse/actors/PulsarActor.java
@@ -1,13 +1,13 @@
 /*
  * Pulsar: lightweight threads and Erlang-like actors for Clojure.
- * Copyright (C) 2013-2015, Parallel Universe Software Co. All rights reserved.
- * 
+ * Copyright (C) 2013-2016 Parallel Universe Software Co. All rights reserved.
+ *
  * This program and the accompanying materials are dual-licensed under
  * either the terms of the Eclipse Public License v1.0 as published by
  * the Eclipse Foundation
- *  
+ *
  *   or (per the licensee's choosing)
- *  
+ *
  * under the terms of the GNU Lesser General Public License version 3.0
  * as published by the Free Software Foundation.
  */
@@ -108,7 +108,7 @@ public class PulsarActor extends Actor<Object, Object> {
 
     @Override
     protected Object filterMessage(Object m) {
-        if(!trap && !(m instanceof ShutdownMessage))
+        if (!trap && !(m instanceof ShutdownMessage))
             m = super.filterMessage(m);
         return convert(m);
     }
@@ -145,7 +145,7 @@ public class PulsarActor extends Actor<Object, Object> {
     private static Keyword keyword(String s) {
         return Keyword.intern(s);
     }
-    
+
     ///////////////// Simple delegates ////////////////////////////
 
 
